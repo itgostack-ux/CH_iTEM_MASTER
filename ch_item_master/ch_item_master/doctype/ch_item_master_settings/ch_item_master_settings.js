7d2bbcd4332c3c6eef89c4f9608065e79997a5f6
Mahalakshmi <mahalakshmi@gostack.in>
2026-07-25 10:22:14 +0530

    Default UOM Changes

---
+++ ch_item_master/ch_item_master/doctype/ch_item_master_settings/ch_item_master_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, GoStack and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CH Item Master Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
